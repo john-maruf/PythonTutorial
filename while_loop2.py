a1e8be4017b71da3bfa2745585569ca622e59553
John Maruf <marufakterjon@gmail.com>
2026-02-19 23:12:30 +0600

    Update while_loop2.py

--- while_loop2.py
+++ while_loop2.py
@@ -1,4 +1,14 @@
-i = 2
-while i <= 10:
-    print(i)
-    i = i + 2
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print(f"{i} x {j} = {i * j}")
+
+
+n = input("Enter a number: ")
+n = int(n)
+
+while n != 0:
+    multiplication_table(n)
+    print("Done")
+    n = input("Enter a number: ")
+    n = int(n)
