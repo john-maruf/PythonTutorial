2e56a08ed126759257fbb9cb6a1b63435c13fa30
John Maruf <marufakterjon@gmail.com>
2026-02-25 23:54:08 +0600

    Create input_age.py

---
+++ input_age.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print(f"Hello {name}, you are an adult.")
+else:
+    print(f"Hello {name}, you are a minor.")
